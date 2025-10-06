5db427a010d28be7b11fd0727bbc98dcabfdbe84
Louis Otter <Ottlouis@icloud.com>
2025-10-06 08:07:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-946072313-2310e",
+  "appId": "1:324483711223:web:9e893b4e25eaa20fea13f2",
+  "apiKey": "AIzaSyCm5iVRj1hOG9NlBtLydsOW-H51d0IdoQs",
+  "authDomain": "studio-946072313-2310e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "324483711223"
+};
